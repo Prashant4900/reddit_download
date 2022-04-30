e4407585f12bcba40b271f2f2ce6324c62e68d03
Prashant Nigam <prashantnigam490@gmail.com>
2022-04-30 20:50:10 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+if (typeof init == "undefined") {
+    const init = function () {
+        const newElement = document.createElement("div", {
+            class: "_3U_7i38RDPV5eBv7m4M-9J",
+            style: "position: fixed; bottom: 0; right: 0; z-index: 99999;",
+        });
+
+        newElement.innerHTML =
+            '<button class="_10K5i7NW6qcm-UoCtpB3aK YszYBnnIoNY8pZ6UwCivd _3yh2bniLq7bYr4BaiXowdO _1EWxiIupuIjiExPQeK4Kud _28vEaVlLWeas1CDiLuTCap"><span class="pthKOcceozMuXLYrLlbL1"><i class="_3yNNYT3e1avhAAWVHd0-92 icon icon-download" id="View--GiveAward--t3_uf8z9d"></i></span><span class="_2-cXnP74241WI7fpcpfPmg _70940WUuFmpHbhKlj8EjZ">Download</span></button>';
+
+        document
+            .getElementsByClassName("_3-miAEojrCvx_4FQ8x3P-s")[1]
+            .appendChild(newElement)
+    };
+
+    init();
+}
+
+
+
+
+// const newElement2 = document.createElement("button", {
+//     class: "_10K5i7NW6qcm-UoCtpB3aK YszYBnnIoNY8pZ6UwCivd _3yh2bniLq7bYr4BaiXowdO _1EWxiIupuIjiExPQeK4Kud _28vEaVlLWeas1CDiLuTCap",
+// });
+// const newElement3 = document.createElement("span", {
+//     class: "pthKOcceozMuXLYrLlbL1",
+// });
+// const newElement4 = document.createElement("i", {
+//     class: "_3yNNYT3e1avhAAWVHd0-92 icon icon-download",
+//     id: "View--GiveAward--t3_uf8z9d",
+// });
+// const newElement5 = document.createElement("span", {
+//     class: "_2-cXnP74241WI7fpcpfPmg _70940WUuFmpHbhKlj8EjZ",
+//     id: "View--GiveAward--t3_uf8z9d",
+// });
+// newElement5.innerHTML = "Download";
+// newElement4.innerHTML = newElement5;
+// newElement3.innerHTML = newElement4;
+// newElement2.innerHTML = newElement3;
+// newElement.innerHTML = newElement2;
